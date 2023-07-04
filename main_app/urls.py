7558ab0fefe32ef42b2ee1b1e8b3632c525969c9
armullen <amber.r.mullen@gmail.com>
2023-07-04 17:47:35 -0500

    adding seeded data

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('about/', views.About.as_view(), name='about'),  
     path('teams/', views.TeamList.as_view(), name='team_list'),
+    path('players/', views.PlayerList.as_view(), name="player_list"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -31,3 +31,46 @@ class TeamList(TemplateView):
         context = super().get_context_data(**kwargs)
         context["teams"] = teams
         return context
+
+class Player(TemplateView):
+    def __init__(self, name, img, position, nickname, years, nationality):
+        self.name = name
+        self.image = img
+        self.position = position
+        self.nickname = nickname
+        self.years = years
+        self.nationality = nationality
+
+players = [
+    # -------------------Red Wings_______________________
+    Player("Pavel Datsyuk", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSx6yw0gZRbebVvlrazcdpFEDxv98m6TaAmg&usqp=CAU", "Center", "The Magic Man", "1996-2021", "Russian"),
+    Player("Gordie Howe", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKCiuXm4Ib3gYGBuZR8sYHkcb-CDNLZ_n63Q&usqp=CAU", "Right Wing", "Mr. Hockey", "1946-1980", "Canadian"),
+    Player("Steve Yzerman", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQURSf3EuIUllOSOaJx4VRNjUkxWecOmp0JGg&usqp=CAU", "Center", "Stevie Y", "1983-2006", "Canadian"),
+    Player("Nick Lindstrom",  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSruuEHv0dC2dag6Zsn1c96Ra-LAVmi1jgfw&usqp=CAU", "Defensmen", "The Perfect Human", "1998-2012", "Sweedish"),
+    # --------------------Bruins---------------------------
+    Player("Bobby Orr", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYNpZCEAfeDwLacYI1qYwln9d_FYrX_3pZZQ&usqp=CAU","Defensmen", "Number 4", "1966-1978", "Canadian"),
+    Player("Ray Bourque", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2tqGug9x1jXdLEksrnPqVyKhvAnlfobjPyQ&usqp=CAU", "Defensmen" "None", "1979-2001", "Canadian"),
+    Player("Patrice Burgeron")
+    Player("Cam Neely")
+    # ---------------------Blackhawks-------------------------------
+    Player("Bobby Hull")
+    Player("Patrick Kane")
+    Player("Chris Chelios")
+    Player("Stan Mikita")
+    # -------------------------Rangers---------------------------
+    Player("Brian Leetch")
+    Player("Mark Messier")
+    Player("Mike Richter")
+    Player("Henrik Lundqvist")
+    # ------------------------Maple Leafs----------------------------
+    Player("Mats Sundin")
+    Player("Doug Gilmore")
+    Player("Wendel Clark")
+    Player("Auston Matthews")
+    # --------------------------Canadians----------------------------
+    Player("Guy Laffleur")
+    Player("Maurice Richard")
+    Player("Patrick Roy")
+    Player("Daly Koivu")
+
+]
