5f49c7ac4d85ddf65f2296070eb9ec0a6ba24ba1
armullen <amber.r.mullen@gmail.com>
2023-07-05 20:23:59 -0500

    getting ready to deploy

--- hockeycollector/settings.py
+++ hockeycollector/settings.py
@@ -22,13 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i%s-csrxb-laa#56)_csm=i9bx9i5j_2op^_*zx6+jt)o_vmw3'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -50,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hockeycollector.urls'
@@ -77,11 +81,11 @@ WSGI_APPLICATION = 'hockeycollector.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hockeycollector',
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:8000/hockeycollector',
+        conn_max_age=600
+    )
     }
-}
 
 
 # Password validation
@@ -119,6 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.COmpressedManifestStaticFilesStorage'
+    
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main_app/static')]
 
 # Default primary key field type
